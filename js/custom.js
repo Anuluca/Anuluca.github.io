694f017850e3c5e68eaadbdb59e306d5b8a4c710
Anuluca <tilucario@outlook.com>
2023-05-29 11:21:31 +0800

    Site updated: 2023-05-29 11:21:30

--- js/custom.js
+++ js/custom.js
@@ -8,82 +8,82 @@
 // var ifDrink = (new Date()).getHours()
 
 $(function () {
-    var str = function () {
-        /*
-             ▄▄▄       ███▄    █  █    ██  ██▓     █    ██  ▄████▄   ▄▄▄      
-    ▒████▄     ██ ▀█   █  ██  ▓██▒▓██▒     ██  ▓██▒▒██▀ ▀█  ▒████▄    
-    ▒██  ▀█▄  ▓██  ▀█ ██▒▓██  ▒██░▒██░    ▓██  ▒██░▒▓█    ▄ ▒██  ▀█▄  
-    ░██▄▄▄▄██ ▓██▒  ▐▌██▒▓▓█  ░██░▒██░    ▓▓█  ░██░▒▓▓▄ ▄██▒░██▄▄▄▄██ 
-     ▓█   ▓██▒▒██░   ▓██░▒▒█████▓ ░██████▒▒▒█████▓ ▒ ▓███▀ ░ ▓█   ▓██▒
-     ▒▒   ▓▒█░░ ▒░   ▒ ▒ ░▒▓▒ ▒ ▒ ░ ▒░▓  ░░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░ ▒▒   ▓▒█░
-      ▒   ▒▒ ░░ ░░   ░ ▒░░░▒░ ░ ░ ░ ░ ▒  ░░░▒░ ░ ░   ░  ▒     ▒   ▒▒ ░
-      ░   ▒      ░   ░ ░  ░░░ ░ ░   ░ ░    ░░░ ░ ░ ░          ░   ▒   
-          ░  ░         ░    ░         ░  ░   ░     ░ ░            ░  ░
-                                                   ░                  
-                                                                                                    
-        */
-    }
-    console.log(str.getMultiLine());
-    let theme = sessionStorage.getItem('theme')
-    if(theme === 'light'){
-        $("#toLightButton").css({ display: "none" })
-        $("#toDarkButton").css({ display: "block" })
-    }else{
-        $("#toLightButton").css({ display: "block" })
-        $("#toDarkButton").css({ display: "none" })
-    }
-    showOrHideToTop()
-})
+  var str = function () {
+    /*
 
-$(window).scroll(function(){
-    showOrHideToTop()
-})
+     _                _                 
+    / \   _ __  _   _| |_   _  ___ __ _ 
+   / _ \ | '_ \| | | | | | | |/ __/ _` |
+  / ___ \| | | | |_| | | |_| | (_| (_| |
+ /_/   \_\_| |_|\__,_|_|\__,_|\___\__,_|
+                                        
+                              
+        */
+  };
+  console.log(str.getMultiLine());
+  let theme = sessionStorage.getItem("theme");
+  if (theme === "light") {
+    $("#toLightButton").css({ display: "none" });
+    $("#toDarkButton").css({ display: "block" });
+  } else {
+    $("#toLightButton").css({ display: "block" });
+    $("#toDarkButton").css({ display: "none" });
+  }
+  showOrHideToTop();
+});
+
+$(window).scroll(function () {
+  showOrHideToTop();
+});
 
 Function.prototype.getMultiLine = function () {
-    var lines = new String(this);
-    lines = lines.substring(lines.indexOf("/*") + 3, lines.lastIndexOf("*/"));
-    return lines;
-}
+  var lines = new String(this);
+  lines = lines.substring(lines.indexOf("/*") + 3, lines.lastIndexOf("*/"));
+  return lines;
+};
 
-function showOrHideToTop(){
-    if($(window).scrollTop()>($('html').height()/2)){
-        $('.to_top').fadeIn(300);
-    } else {
-        $('.to_top').fadeOut(200);
-    }
+function showOrHideToTop() {
+  if ($(window).scrollTop() > $("html").height() / 2) {
+    $(".to_top").fadeIn(300);
+  } else {
+    $(".to_top").fadeOut(200);
+  }
 }
 
 //gitalk&valine双评论端
 $(".gitalk_btn").click(function () {
-    $("#vcomments").css({ display: "none" });
-    $("#gitalk-container").css({ display: "block" });
+  $("#vcomments").css({ display: "none" });
+  $("#gitalk-container").css({ display: "block" });
 
-    $(".valine_btn").css({ color: "black" })
-    $(".gitalk_btn").css({ color: "blue" })
-    $(".moving").animate({
-        "width": "180px",
-        "marginLeft": "120px"
-    });
+  $(".valine_btn").css({ color: "black" });
+  $(".gitalk_btn").css({ color: "blue" });
+  $(".moving").animate({
+    width: "180px",
+    marginLeft: "120px",
+  });
 });
 $(".valine_btn").click(function () {
-    $("#gitalk-container").css({ display: "none" });
-    $("#vcomments").css({ display: "block" });
+  $("#gitalk-container").css({ display: "none" });
+  $("#vcomments").css({ display: "block" });
 
-    $(".valine_btn").css({ color: "blue" })
-    $(".gitalk_btn").css({ color: "white" })
-    $(".moving").animate({
-        "width": "120px",
-        "marginLeft": "0px"
-    });
+  $(".valine_btn").css({ color: "blue" });
+  $(".gitalk_btn").css({ color: "white" });
+  $(".moving").animate({
+    width: "120px",
+    marginLeft: "0px",
+  });
 });
 
 //右下角链接互动
-$(".link_l").mouseenter(function () {
-    $(this).stop().css({ backgroundColor: "white" })
-    $(this).find("span").css({ color: "#3B3E40" })
-    $(this).find("div").css({ "background-position": "0 -29px" })
-}).stop().mouseleave(function () {
-    $(this).stop().css({ backgroundColor: "rgba(0, 0, 0, 0.5)", border: "0" })
-    $(this).find("span").css({ color: "white" })
-    $(this).find("div").css({ "background-position": "0 0" })
-})
+$(".link_l")
+  .mouseenter(function () {
+    $(this).stop().css({ backgroundColor: "white" });
+    $(this).find("span").css({ color: "#3B3E40" });
+    $(this).find("div").css({ "background-position": "0 -29px" });
+  })
+  .stop()
+  .mouseleave(function () {
+    $(this).stop().css({ backgroundColor: "rgba(0, 0, 0, 0.5)", border: "0" });
+    $(this).find("span").css({ color: "white" });
+    $(this).find("div").css({ "background-position": "0 0" });
+  });
